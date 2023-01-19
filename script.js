da3cdaf57f3580e27ce86bbf4b94377c9a3bbb8a
benjamin farmer <viroben2001@gmail.com>
2023-01-18 23:49:49 -0800

    finished code

--- script.js
+++ script.js
@@ -1,58 +1,110 @@
 // DOM elements
 const valueEl = document.getElementById('value')
+const customInput = document.getElementById('custom-input')
 const plusBtn = document.getElementById('plus')
 const minusBtn = document.getElementById('minus')
+const plusFiveBtn = document.getElementById('plusFive')
+const minusFiveBtn = document.getElementById('minusFive')
+const addCustomBtn = document.getElementById('input-submit')
+const incrementOddBtn = document.getElementById('increment-odd')
+const incrementAsyncBtn = document.getElementById('increment-async')
 
-// initial state value
+// initial State
 const initialState = {
-    value: 0
+    value: 0,
+    
 }
-
-// reducer
 const counterReducer = (state = initialState, action) => {
     switch (action.type) {
         case 'counter/incremented':
             return { value: state.value + 1 }
         case 'counter/decremented':
             return { value: state.value - 1 }
+        case 'counter/plusFive':
+          return { value: state.value + 5 }
+        case 'counter/minusFive':
+          return { value: state.value - 5 }
+        case 'counter/incrementOdd':
+          if(state.value % 2 == 0) {
+            return state
+          } else {
+            return { value: state.value + 1 }
+          }
+          case 'counter/custom':
+            return { value: state.value + action.payload }
         default:
         return state
     }
 }
-
 // action object definitions
 const addAction = {
-  type: 'counter/incremented'
-}
-
-const subAction = {
-  type: 'counter/decremented'
-}
-
-// generating the store
-let store = Redux.createStore(counterReducer)
-
-// defining render
+    type: 'counter/incremented'
+  }
+  
+  const subAction = {
+    type: 'counter/decremented'
+  }
+  
+  const addFiveAction = {
+    type: 'counter/plusFive'
+  }
+  
+  const subFiveAction = {
+    type: 'counter/minusFive'
+  }
+  
+  const incrementOddAction = {
+    type: 'counter/incrementOdd'
+  }
+// declaration of Reducer
+	let store = Redux.createStore(counterReducer)
+// define render
 const render = () => {
     const state = store.getState()
+    
     valueEl.innerHTML = state.value.toString()
 }
-
-// establishing dispatch functions
 const addOne = () => {
-  store.dispatch(addAction)
+    store.dispatch(addAction)
+  }
+	 const subOne = () => {
+    store.dispatch({type: 'counter/decremented'})
 }
 
-const subOne = () => {
-  store.dispatch(subAction)
+const addFive = () => {
+    store.dispatch({type: 'counter/custom', payload: 5})
 }
 
-// event listeners
-plusBtn.addEventListener('click', addOne)
+const subFive = () => {
+    store.dispatch({type: 'counter/custom', payload: -5})
+}
+const incrementAsync = () => {
+    setTimeout(() => {
+      store.dispatch(addAction)
+    }, 1000);
+  }
+const incrementOdd = () => {
+    store.dispatch(incrementOddAction)
+  }
+  const addCustom = () => {
+    let num = +customInput.value
+    store.dispatch({
+      type: 'counter/custom',
+      payload: num
+    })
+  }
+
+// adding event listeners to dispatch actions
 minusBtn.addEventListener('click', subOne)
-
-// initial render
+plusBtn.addEventListener('click', addOne)
+plusFiveBtn.addEventListener('click', addFive)
+minusFiveBtn.addEventListener('click', subFive)
+addCustomBtn.addEventListener('click', addCustom)
+incrementAsyncBtn.addEventListener('click', incrementAsync)
+incrementOddBtn.addEventListener('click', incrementOdd)
+// call initial render
 render()
+    
 
-// subscribe reruns render on dispatch
-store.subscribe(render)
+// re-render every time state is changed.
+store.subscribe(render);
